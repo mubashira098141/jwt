4aaa6c1fb7faada419e83eb7a0c0d801419a7dcb
Your Name <youremail@example.com>
2025-02-16 21:21:29 +0530

    initial commit

--- src/main/java/com/jwt/example/controller/HomeController.java
+++ src/main/java/com/jwt/example/controller/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/hoo")
+@RequestMapping("/api/demo")
 public class HomeController {
 
     @Autowired
